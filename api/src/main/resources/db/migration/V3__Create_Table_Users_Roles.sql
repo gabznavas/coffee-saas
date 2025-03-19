77c878d53a4cdc0f6b214c6a904564d0baf184c8
gabznavas <gabznavas@proton.me>
2025-03-19 09:29:48 -0300

    create table users roles

---
+++ api/src/main/resources/db/migration/V3__Create_Table_Users_Roles.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS `coffee-db`.`users_roles` (
+  `role_id` BIGINT NOT NULL,
+  `user_id` BIGINT NOT NULL,
+  PRIMARY KEY (`role_id`, `user_id`),
+  INDEX `fk_users_roles_users1_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_users_roles_roles`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `coffee-db`.`roles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_roles_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `coffee-db`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB
