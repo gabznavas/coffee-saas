65f6a9bc943fa15fa88e7ef50cca59e5be426e73
gabznavas <gabznavas@proton.me>
2025-03-21 22:14:48 -0300

    add router link header

--- ui/src/app/header/header.module.ts
+++ ui/src/app/header/header.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MenuRightComponent } from './menu-right/menu-right.component';
 import { ComponentsModule } from '../components/components.module';
-
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -14,6 +14,7 @@ import { ComponentsModule } from '../components/components.module';
   imports: [
     CommonModule,
     ComponentsModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent
